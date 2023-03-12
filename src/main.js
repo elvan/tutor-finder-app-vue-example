bc1f4a6b467ef893136970a8dbfcd12f70befe1b
Elvan Hidayat <elvanhd@gmail.com>
2023-03-12 19:52:09 +0700

    render a loading spinner

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseCard from './components/ui/BaseCard.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
 import router from './router.js';
 import store from './store/index.js';
 
@@ -15,5 +16,6 @@ app.use(store);
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
 
 app.mount('#app');
