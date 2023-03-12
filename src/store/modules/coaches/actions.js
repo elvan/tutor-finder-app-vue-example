321b69c07da9fdf9f2475d9bd4b002e1ccfd1bfb
Elvan Hidayat <elvanhd@gmail.com>
2023-03-12 20:52:22 +0700

    cache http response data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -27,7 +27,11 @@ export default {
       id: userId,
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(`${dbUrl}/coaches.json`);
     const responseData = await response.json();
 
@@ -50,5 +54,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   },
 };
