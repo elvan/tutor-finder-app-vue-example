14721a53c7d73641c5ba5b7dede1ec9a5f8c0fe7
Elvan Hidayat <elvanhd@gmail.com>
2023-03-12 12:19:43 +0700

    add vuex and coach data

---
+++ src/store/modules/coaches/index.js
@@ -0,0 +1,34 @@
+import actions from './actions.js';
+import getters from './getters.js';
+import mutations from './mutations.js';
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Aaron',
+          lastName: 'Ferguson',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm Aaron and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30,
+        },
+        {
+          id: 'c2',
+          firstName: 'Billy',
+          lastName: 'Walker',
+          areas: ['frontend', 'career'],
+          description:
+            'I am Billy and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30,
+        },
+      ],
+    };
+  },
+  mutations,
+  actions,
+  getters,
+};
