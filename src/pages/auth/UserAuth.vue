d4d92c3373a5ccd8f9697fddfae209b1ca0d0a34
Elvan Hidayat <elvanhd@gmail.com>
2023-03-13 05:14:58 +0700

    route navigation guards

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -82,6 +82,8 @@ export default {
         } else {
           await this.$store.dispatch('signup', actionPayload);
         }
+        const redirectUrl = '/' + (this.$route.query.redirect || 'coaches');
+        this.$router.replace(redirectUrl);
       } catch (err) {
         this.error = err.message || 'Failed to authenticate, try later.';
       }
