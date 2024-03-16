9caa2ac105aaebcd62d44f8d4aa648855dd85951
Elvan Hidayat <elvanhd@gmail.com>
2024-03-16 22:41:06 +0700

    Implement signup functionality and Vuex auth module
    - Enable signup functionality in UserAuth component
    - Dispatch 'signup' action with user email and password
    - Introduce auth module in Vuex store
    - Add actions for signup with async request to Firebase Auth
    - Store user data upon successful signup
    - Setup getters and mutations for auth module
    - Handle user data and authentication token
    - Update store index to include the auth module
    - Extract userId from auth module for consistent access across the app

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,34 @@
+export default {
+  login() {},
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        process.env.VUE_APP_FIREBASE_API_KEY,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
