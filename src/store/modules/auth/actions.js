ebf8cf63eb66323206ed6dbb9aa25db09b3ce433
Elvan Hidayat <elvanhd@gmail.com>
2024-03-16 23:01:02 +0700

    Implement login functionality and refine signup
    - Add login action to auth module for handling user login
    - Fetch API call to Firebase Authentication for login
    - Handle successful and error responses
    - Refine signup action to include detailed error handling
    - Update UserAuth.vue to dispatch login or signup based on mode
    - Create actionPayload object to streamline payload creation
    - Implement async-await for dispatch calls within try-catch for error handling
    - Enhance error messages for better user feedback during authentication failures

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,35 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+        process.env.VUE_APP_FIREBASE_API_KEY,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate. Check your login data.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const response = await fetch(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
@@ -19,7 +49,7 @@ export default {
     if (!response.ok) {
       console.log(responseData);
       const error = new Error(
-        responseData.message || 'Failed to authenticate.'
+        responseData.message || 'Failed to authenticate. Check your login data.'
       );
       throw error;
     }
