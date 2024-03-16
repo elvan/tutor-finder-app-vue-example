20858e635d6997c25f90fd5310421029e7e98e36
Elvan Hidayat <elvanhd@gmail.com>
2024-03-16 23:45:28 +0700

    Improve authentication with automatic login and refactoring
    - Implement automatic login on app creation to persist user session
    - App.vue now dispatches 'tryLogin' action during the created lifecycle hook
    - Refactor authentication actions for login and signup to use a common 'auth' method
    - Centralizes API request logic in 'auth' action within auth module, improving code reuse
    - Store authentication tokens and user ID in localStorage for persistence
    - 'auth' action now saves token and userId in localStorage upon successful authentication
    - Add 'tryLogin' action to check for existing login data in localStorage and commit user state
    - Enhances user experience by maintaining authentication state across sessions
    - Simplify 'login' and 'signup' actions to dispatch 'auth' with appropriate mode ('login' or 'signup')
    - Streamlines the process of handling user authentication by consolidating common logic

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,65 +1,65 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
-        process.env.VUE_APP_FIREBASE_API_KEY,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+      process.env.VUE_APP_FIREBASE_API_KEY;
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        process.env.VUE_APP_FIREBASE_API_KEY;
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(
         responseData.message || 'Failed to authenticate. Check your login data.'
       );
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn,
     });
   },
-  async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
-        process.env.VUE_APP_FIREBASE_API_KEY,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
 
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.message || 'Failed to authenticate. Check your login data.'
-      );
-      throw error;
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null,
+      });
     }
-
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
-    });
   },
   logout(context) {
     context.commit('setUser', {
