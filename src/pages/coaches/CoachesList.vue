93f716592e02574968e51c1ddb6c4ab71af676eb
Elvan Hidayat <elvanhd@gmail.com>
2024-03-16 23:29:17 +0700

    Enhance navigation and authentication flow
    - Redirect user to coaches list after successful logout or signup
    - Modification in TheHeader component to navigate to /coaches on logout
    - UserAuth.vue now redirects to a specific page or coaches list after login/signup
    - Introduce navigation guards to restrict access based on authentication status
    - CoachesList.vue checks for user's authentication status before showing "Register as Coach"
    - Implement global beforeEach guard in router.js to handle redirects based on requiresAuth and requiresUnauth meta fields
    - Ensure smooth user experience by directing users appropriately based on their authentication state, either prompting login for protected routes or redirecting logged-in users away from auth page

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -16,7 +16,13 @@
           <base-button mode="outline" @click="loadCoaches(true)"
             >Refresh</base-button
           >
-          <base-button v-if="!isCoach && !isLoading" link to="/register"
+          <base-button link to="/auth?redirect=register" v-if="!isLoggedIn"
+            >Login to Register as Coach</base-button
+          >
+          <base-button
+            v-if="isLoggedIn && !isCoach && !isLoading"
+            link
+            to="/register"
             >Register as Coach</base-button
           >
         </div>
@@ -61,6 +67,9 @@ export default {
     };
   },
   computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    },
     isCoach() {
       return this.$store.getters['coaches/isCoach'];
     },
